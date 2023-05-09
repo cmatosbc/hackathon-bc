834d1a9a58335ac6b359d02b5625366dc2b06b62
cmatosbc <cmatos@bettercollective.com>
2023-05-09 10:10:08 +0100

    [T005] Parsed response - team ID, name and code just.

--- src/request.js
+++ src/request.js
@@ -2,8 +2,8 @@ import { apiKey, apiHost } from './config.js';
 
 export default function ( term ) {
 	var myHeaders = new Headers();
-	myHeaders.append("X-RapidAPI-Key", "784166e8damshc6d584f31ac3909p179b89jsn9d778e3690a7");
-	myHeaders.append("X-RapidAPI-Host", "api-football-v1.p.rapidapi.com");
+	myHeaders.append("X-RapidAPI-Key", apiKey);
+	myHeaders.append("X-RapidAPI-Host", apiHost);
 	myHeaders.append("Content-type", "application/json");
 
 	var requestOptions = {
